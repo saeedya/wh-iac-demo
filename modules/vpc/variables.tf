1a12a492c24bcf5d4bf862b35f89d08d0fabbe44
Saeed <yasrebi.saeed@gmail.com>
2024-11-03 01:41:35 +0000

    adding vpc for nrtworks, subnet,...

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {}
+variable "region" {}
+variable "project_name" {}
+variable "public_subnet_1a_cidr" {}
+variable "public_subnet_2b_cidr" {}
+variable "private_subnet_3a_cidr" {}
+variable "private_subnet_4b_cidr" {}
+variable "private_subnet_5a_cidr" {}
+variable "private_subnet_6b_cidr" {}
